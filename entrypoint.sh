80489c87db9c51dda2509385f60c7aa6598ce19c
francisco-codefresh <francisco@codefresh.io>
2018-07-04 11:45:38 -0600

    Adding INF msg to all echoes

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 msg() { echo -e "INF---> $1"; }
 err() { echo -e "ERR---> $1" ; exit 1; }
 
-printenv
+#printenv
 JSON_PAYLOAD={\"tag\":{\"registry\":\"$TL_REGISTRY\",\"repo\":\"$TL_IMAGE_NAME\",\"tag\":\"$TL_IMAGE_TAG\"}}
 echo $JSON_PAYLOAD
 
@@ -18,21 +18,21 @@ until [ "$SCAN_FINISH_STATUS" = "completed" ]; do
   scan_current_status=$(curl -X GET -k -s -u $TL_CONSOLE_USERNAME:$TL_CONSOLE_PASSWORD -G https://$TL_CONSOLE_HOSTNAME:$TL_CONSOLE_PORT/api/v1/registry -d search=$TL_IMAGE_NAME:$TL_IMAGE_TAG -d limit=1 | cut -c5-6)
   if [ "$scan_current_status" = id ]; then  
     SCAN_FINISH_STATUS="completed"
-    echo "Scan completed"
+    msg "Scan completed"
   else
     SCAN_FINISH_STATUS="Running scan"
-    echo $SCAN_FINISH_STATUS
+    msg $SCAN_FINISH_STATUS
   fi
 done
 
 REPORT_NAME=$(echo ''$TL_IMAGE_NAME:$TL_IMAGE_TAG | tr /: _)
 curl -X GET -ks -u $TL_CONSOLE_USERNAME:$TL_CONSOLE_PASSWORD -G https://$TL_CONSOLE_HOSTNAME:$TL_CONSOLE_PORT/api/v1/registry -d search=$TL_IMAGE_NAME:$TL_IMAGE_TAG -d limit=1 -o TL_report_$REPORT_NAME.json
-echo "Report Downloaded"
+msg "Report Downloaded"
 
 COMPLIANCE_ISSUES=$(cat TL_report_$REPORT_NAME.json | jq ".[0].info.complianceDistribution.$TL_COMPLIANCE_THRESHOLD")
 CVEVULNERABILITY_ISSUES=$(cat TL_report_$REPORT_NAME.json | jq ".[0].info.cveVulnerabilityDistribution.$TL_VULNERABILITY_THRESHOLD")
 if [ $COMPLIANCE_ISSUES -gt 0 ]; then 
-  echo "COMPLIANCE_THRESHOLD EXEECED = $COMPLIANCE_ISSUES $TL_COMPLIANCE_THRESHOLD-severity issue(s) found"
+  msg "COMPLIANCE_THRESHOLD EXEECED = $COMPLIANCE_ISSUES $TL_COMPLIANCE_THRESHOLD-severity issue(s) found"
   exit 1
 else
   echo "COMPLIANCE CHECK=PASSED"
